8a1493f13c919f0fb3d0381073f01df8c0577a26
Du Haonan <1353592296@qq.com>
2023-10-23 20:51:59 +0800

    27集

---
+++ my_program/pages/data/data.js
@@ -0,0 +1,97 @@
+// pages/data/data.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        info: "Hello world",
+        imageSrc: "/images/test.jpg",
+        randomNum: Math.random().toFixed(2),
+        num: 1,
+        msg:"你好,",
+        type:3,
+        arr1:['苹果','华为','小米'],
+        userlist1:[
+            {id:1,name:"1111"},
+            {id:2,name:"2222"},
+            {id:3,name:"3333"},
+        ]
+    },
+    btnTapHandler(e) {
+        console.log(this.data.num)
+        this.setData({
+            num: this.data.num + 1
+        })
+    },
+    btnTapHandler2(e){
+        var n = e.target.dataset.info;
+
+        this.setData({
+            num:this.data.num + n
+        })
+    },
+    inputHandler(e){
+        
+        this.setData({
+            msg: e.detail.value
+        })
+        console.log(e.detail.value)
+        console.log(this.data.msg)
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
