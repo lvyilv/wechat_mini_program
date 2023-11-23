598bd80c71b11c1a582248d9a6c6dc9aa88403a7
Du Haonan <1353592296@qq.com>
2023-11-23 14:59:27 +0800

    实现产品详情页

--- e-commerce_demo/components/productIntroduction/productIntroduction.js
+++ e-commerce_demo/components/productIntroduction/productIntroduction.js
@@ -1,27 +1,29 @@
 // components/productsIntroduction/productIntroduction.js
 Component({
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    shopInfo: JSON
+  },
 
-    /**
-     * 组件的属性列表
-     */
-    properties: {
-      shopInfo:JSON
-    },
+  /**
+   * 组件的初始数据
+   */
+  data: {
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
+  },
 
-    },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-
-    },
-    show(){
-      console.log(this.properties.shopInfo)
-    }
+  },
+  show() {
+    console.log(this.properties.shopInfo)
+  }
 })
